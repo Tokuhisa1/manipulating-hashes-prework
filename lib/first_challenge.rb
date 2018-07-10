08d2fd50fecaa3d4fc981586b83ed23ab080dad9
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-10 23:38:31 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,8 +15,7 @@ def first_challenge
     }
   }
   # Your code here
-  contacts["Freddy Mercury"]
-  [:favorite_icecream_flavors].delete_if {|flavor| flavor == "strawberry"}
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|flavor| flavor == "strawberry"}
 # Remember to return your newly altered contacts hash!
   contacts
 end
